d58eed007b75601c43ce1a78220b3aab7c9a1d45
Brandon <devbrandoncraig@gmail.com>
2020-02-26 14:23:25 -0700

    Almost 1/2 Way Done With Course

---
+++ search.php
@@ -0,0 +1,29 @@
+<?php
+include("db.php");
+
+$search = $_POST['search'];
+
+if(!empty($search)) {
+    $query = "SELECT * FROM cars WHERE cars LIKE '$search%'";
+    $search_query = mysqli_query($connection, $query);
+    
+    if(!$search_query) {
+        die("QUERY FAILED" . mysqli_error($connection));
+    }
+    
+    while($row = mysqli_fetch_array($search_query)) {
+        $brand = $row['cars'];
+        ?>
+<ul class="list-unstyled">
+
+    <?php
+            
+        echo "<li>{$brand} in stock</li>";        
+
+    ?>
+
+</ul>
+<?php }
+}
+
+?>
